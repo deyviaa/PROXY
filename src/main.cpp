4c9ffa5b738bd0dad4d1a957733cdbfbb55482ac
Legokid <legoked@gmail.com>
2025-05-06 11:56:31 -0400

    final commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <u8g2lib.h>
+#include <ESP32Servo.h>
+
 
 // VCNL4200 proximity sensor address
 #define VCNL4200_ADDR 0x51
@@ -20,6 +22,25 @@
 #define OLED_CS D10
 #define OLED_DC D9
 #define OLED_RST D8
+
+// OLED icon draw
+void drawHazardIcon(U8G2 &u8g2) {
+  // triangle vertices
+  uint8_t x0 = 64, y0 = 20;  // top
+  uint8_t x1 = 44, y1 = 50;  // bottom left
+  uint8_t x2 = 84, y2 = 50;  // bottom right
+
+  // draw hollow triangle
+  u8g2.drawLine(x0, y0, x1, y1);
+  u8g2.drawLine(x1, y1, x2, y2);
+  u8g2.drawLine(x2, y2, x0, y0);
+
+  // draw exclamation mark inside
+  u8g2.setFont(u8g2_font_helvB14_tr);
+  u8g2.drawStr(62, 45, "!");
+  
+}
+
 
 // OLED U8g2 instance
 U8G2_SH1106_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, OLED_CS, OLED_DC, OLED_RST);
@@ -74,6 +95,71 @@ uint16_t readProximity() {
   return (high << 8) | low;
 }
 
+void SET_OLED_DISPLAY(uint8_t stage, uint16_t prx) {
+  
+  switch (stage) {
+    case CLEAR:
+    u8g2.clearBuffer();
+    u8g2.drawFrame(0, 0, 128, 64);
+    u8g2.setFont(u8g2_font_helvB14_tr);
+    u8g2.setCursor(20,30);
+    u8g2.print("GOOD TO");
+    u8g2.setCursor(38,50);
+    u8g2.print("GO :D");
+    u8g2.sendBuffer();
+      break;
+    case BLUE:
+    u8g2.clearBuffer();
+    u8g2.drawFrame(0, 0, 128, 64);
+    u8g2.setFont(u8g2_font_freedoomr10_tu);
+    u8g2.setCursor(32,30);
+    u8g2.print("OBSTACLE");
+    u8g2.setCursor(32,50);
+    u8g2.print("DETECTED");
+    u8g2.sendBuffer();
+      break;
+    case YELLOW:
+    u8g2.clearBuffer();
+    u8g2.drawFrame(0, 0, 128, 64);
+    u8g2.setFont(u8g2_font_helvB14_tr);
+    u8g2.setCursor(5,38);
+    u8g2.print("!! CAUTION !!");
+    u8g2.sendBuffer();
+      break;
+    case RED:
+    u8g2.clearBuffer();
+    u8g2.drawFrame(0, 0, 128, 64);
+    u8g2.setFont(u8g2_font_helvB14_tr);
+    u8g2.setCursor(13,30);
+    u8g2.print("COLLISION");
+    u8g2.setCursor(18,50);
+    u8g2.print("IMMINENT");
+    u8g2.sendBuffer();
+   
+      break;
+    case CRITICAL:
+    u8g2.clearBuffer();
+    u8g2.drawFrame(0, 0, 128, 64);
+    u8g2.setFont(u8g2_font_helvB14_tr);
+    u8g2.setCursor(3,20);
+    u8g2.print("WATCH OUT!");
+    u8g2.setCursor(20,40);
+    drawHazardIcon(u8g2);
+    u8g2.sendBuffer();
+    delay(100);
+    u8g2.clearBuffer();
+    u8g2.drawFrame(0, 0, 128, 64);
+    u8g2.setFont(u8g2_font_helvB14_tr);
+    u8g2.setCursor(3,20);
+    u8g2.print("");
+    u8g2.setCursor(20,40);
+    drawHazardIcon(u8g2);
+    u8g2.sendBuffer();
+      break;
+  }
+
+
+}
 // Function to set the LED stage
 // 0 = clear, 1 = blue, 2 = yellow, 3 = red, 4 = critical
 void SET_STAGE(uint8_t stage) {
@@ -125,26 +211,13 @@ void loop() {
   else if (prox >= THRESH_RED)
   STAGE = CRITICAL;
   
+  Serial.print("Proximity: ");
+  delay(5);
+  Serial.println(prox);
+  delay(5);
+
   SET_STAGE(STAGE);
-  
-    if (prox > 5) {
-    Serial.print("Proximity: ");
-    delay(5);
-    Serial.println(prox);
-    delay(5);
-  
-    // Display on OLED
-  
-    u8g2.clearBuffer();
-    u8g2.drawFrame(0, 0, 128, 64);
-    u8g2.setFont(u8g2_font_freedoomr10_tu);
-    u8g2.setCursor(10,20);
-    u8g2.print("STAGE:");
-    u8g2.setCursor(10,40);
-    u8g2.print(STAGE);
-    u8g2.drawBox(10, 50, map(prox, 0, 65535, 100, 0), 10);
-    u8g2.sendBuffer();
-    }
+  SET_OLED_DISPLAY(STAGE, prox);
 
   delay(100);
   }
